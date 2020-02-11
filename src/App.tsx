fb6822b9e365fdd30a1f2bb2c179f9751b5da66c
fjuriolli <fjuriolli@gmail.com>
2020-02-11 00:34:44 -0300

    Adding graphql to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import Amplify from "aws-amplify";
 import aws_exports from "./aws-exports";
 import Routes from "./routes/routes";
@@ -9,15 +11,21 @@ import light from "./styles/themes/light";
 
 Amplify.configure(aws_exports);
 
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
+
 const App: React.StatelessComponent<{}> = () => {
   return (
-    <ThemeProvider theme={light}>
-      <div className="App">
-        <GlobalStyle />
-        <Header />
-        <Routes />;
-      </div>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={light}>
+        <div className="App">
+          <GlobalStyle />
+          <Header />
+          <Routes />;
+        </div>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 
